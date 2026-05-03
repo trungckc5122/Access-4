be437034e6a20820016df4ebe0ad1805bf9ee955
trungckc5122 <trungckc5122@gmail.com>
2026-05-03 13:17:07 +0700

    Add files via upload

--- core-listening-ket.js
+++ core-listening-ket.js
@@ -1417,7 +1417,7 @@ class ListeningCore {
 
         const prevPartBtn = document.createElement('button');
         prevPartBtn.className = 'nav-arrow-btn nav-prev-part';
-        prevPartBtn.title = 'Part trước';
+        prevPartBtn.title = 'Previous Part';
         prevPartBtn.innerHTML = `
             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="15 18 9 12 15 6"></polyline></svg>
             <span>Previous Part</span>
@@ -1445,7 +1445,7 @@ class ListeningCore {
 
         const nextPartBtn = document.createElement('button');
         nextPartBtn.className = 'nav-arrow-btn nav-next-part';
-        nextPartBtn.title = 'Part tiếp theo';
+        nextPartBtn.title = 'Next Part';
         nextPartBtn.innerHTML = `
             <span>Next Part</span>
             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="9 18 15 12 9 6"></polyline></svg>
--- core-reading-ket.js
+++ core-reading-ket.js
@@ -1695,10 +1695,10 @@ class ReadingCore {
 
         const prevPartBtn = document.createElement('button');
         prevPartBtn.className = 'nav-arrow-btn nav-prev-part';
-        prevPartBtn.title = 'Part trước';
+        prevPartBtn.title = 'Previous Part';
         prevPartBtn.innerHTML = `
             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="15 18 9 12 15 6"></polyline></svg>
-            <span>Part trước</span>
+            <span>Previous Part</span>
         `;
         if (part <= 1) prevPartBtn.disabled = true;
         else prevPartBtn.addEventListener('click', () => {
@@ -1723,9 +1723,9 @@ class ReadingCore {
 
         const nextPartBtn = document.createElement('button');
         nextPartBtn.className = 'nav-arrow-btn nav-next-part';
-        nextPartBtn.title = 'Part sau';
+        nextPartBtn.title = 'Next Part';
         nextPartBtn.innerHTML = `
-            <span>Part sau</span>
+            <span>Next Part</span>
             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="9 18 15 12 9 6"></polyline></svg>
         `;
         if (part >= 5) nextPartBtn.disabled = true;
